0d6fa34ba3bc77d5335ec1dcb35cf7345c51f850
Aryan Chouksey <ayushchauksey08@gmail.com>
2024-07-17 20:19:11 +0530

    LogIn, Home and Profile done.

--- app/index.jsx
+++ app/index.jsx
@@ -33,11 +33,11 @@ export default function App() {
             Find homes at best rates!
           </Text>
 
-          {/* <CustomButton 
+          <CustomButton 
             title="Continue with email"
             handlePress={() => router.push('/sign-in')}
             containerStyles="w-full mt-7"
-          /> */}
+          />
         </View>
       </ScrollView>
 
